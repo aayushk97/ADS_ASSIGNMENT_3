059efdcbf23826d73c86dc148524469230b81174
Aayush Kumar Singh <67497932+aayushk97@users.noreply.github.com>
2021-04-21 23:26:25 +0530

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,3 +1,13 @@
 class Block{
+	private String currentHash;
+	private String prevHash;
+	private int noice;
+	
+	private Vector<Transcation> transactionList;
+	
+	public Block(){
+		this.transactionList = new Vector<Transaction>();
+	
+	}
 	
 }
