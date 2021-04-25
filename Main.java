bb9f3a98e8dee88481d8545ee3ad818859ff544e
Aayush Kumar Singh <67497932+aayushk97@users.noreply.github.com>
2021-04-25 23:49:45 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,14 +5,29 @@ public class main{
 	public static w = 4;  //first w bits to be zero
 	public static int numNodes;
 	public static Vector<Node> nodes;
-
+	
+	static Vector<Queue<Block>> blockReceivingQueues;
+	static Vector<Queue<Transaction>> txnReceivingQueues;
+	
 	public static void main(String[] args){
+	
+	
 	Scanner in = new Scanner(System.in);
 	//create a new block chain class
 	//create new nodes
 	System.out.println("Enter the number of nodes in network");
 	numNodes = in.nextInt();
 
+	//initialize the queues of each node 
+	blockReceivingQueues = new Vector<>();
+	txnReceivingQueues = new Vector<>();
+	
+	for(int i = 0; i < numNodes; i++){
+		blockReceivingQueues.add(new LinkedList<>());
+		txnReceivingQueues.add(new LinkedList<>());
+	
+	}
+	
 	//create a node
 	//create genesis block using node and add it to blockchain
 
