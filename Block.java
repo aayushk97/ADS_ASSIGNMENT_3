64ab24d1e4aa93099561aeeced67621c7e72149f
Aayush Kumar Singh <67497932+aayushk97@users.noreply.github.com>
2021-04-26 15:46:37 +0530

    Update Block.java

--- Block.java
+++ Block.java
@@ -19,7 +19,7 @@ class Block{
 	}
 	
 	public Block(Vector<Transaction> vec){	//This constructor is for genesis block only
-		byte[] genesis = new byte[32] //256 because hash size is 256bit
+		byte[] genesis = new byte[32]; //256 because hash size is 256bit
 		this.prevBlockHash = genesis;
 		this.merkleTree = new Merkle(vec);  //vec is the list of all transaction related to this block		
 		//calculate hash of this block using prevHash + nonce + Txns + timeStamp
@@ -34,23 +34,28 @@ class Block{
 	public byte[] getBlockInFormOfbytes(){ //leaving nonce the part that needs to be taken as input to hash
 		ByteArrayOutputStream s = new ByteArrayOutputStream();
 		//s.write()
+		try{
 		s.write(prevBlockHash);
 		s.write(merkleTree.rootHash);
+		}catch(Exception e){
+			throw new RuntimeException (e);
+		}
 		return s.toByteArray();
 	}
 
 
 	//Not needed for now. keep it by end we can remove this
 	public byte[] longToByte(long inp){
-		//try{
+		try{
 			ByteArrayOutputStream outOS = new ByteArrayOutputStream();
 			DataOutputStream dataOS = new DataOutputStream(outOS);
 			dataOS.writeLong(inp);
 			byte[] bytes = outOS.toByteArray();
 			dataOS.close();
 			return bytes;
-		//}catch()
-		
+		}catch(Exception e){
+			throw new RuntimeException (e);
+		}
 	}
 
 
